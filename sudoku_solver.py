f7f398f607da288f730c29d5ed33ee9906b0378a
jojosephwongck <111182417+jojosephwongck@users.noreply.github.com>
2022-11-05 15:38:34 +0800

    Add files via upload

---
+++ sudoku_solver.py
@@ -0,0 +1,61 @@
+A = [["5","3",".",".","7",".",".",".","."],
+     ["6",".",".","1","9","5",".",".","."],
+     [".","9","8",".",".",".",".","6","."],
+     ["8",".",".",".","6",".",".",".","3"],
+     ["4",".",".","8",".","3",".",".","1"],
+     ["7",".",".",".","2",".",".",".","6"],
+     [".","6",".",".",".",".","2","8","."],
+     [".",".",".","4","1","9",".",".","5"],
+     [".",".",".",".","8",".",".","7","9"]]
+
+A2 = [[5, 3,".",".", 7,".",".",".","."],
+     [6,".",".", 1, 9, 5,".",".","."],
+     [".",9,8,".",".",".",".",6,"."],
+     [8,".",".",".",6,".",".",".",3],
+     [4,".",".",8,".",3,".",".",1],
+     [7,".",".",".",2,".",".",".",6],
+     [".",6,".",".",".",".",2,8,"."],
+     [".",".",".",4,1,9,".",".",5],
+     [".",".",".",".",8,".",".",7,9]]
+
+def find_empty(matrix):
+     for i in range(9):
+          for j in range(9):
+               # empty slot denoted by '.'
+               if matrix[i][j] == '.':
+                    return i , j
+     return None, None
+
+def check_valid(guess, row, col, matrix):
+     # keep if numbers are in string format
+     guess = str(guess)
+
+     if guess in matrix[row]:
+          return False
+
+     for i in range(9):
+          if guess == matrix[i][col]:
+               return False
+
+     i = row // 3
+     j = col // 3
+     square = [matrix[3*i + a][3*j + b] for a in range(3) for b in range(3)]
+     if guess in square:
+          return False
+
+     return True
+
+def solver(matrix):
+     row, col = find_empty(matrix)
+     if row is None:
+          return True
+
+     for guess in range(1, 10):
+          if check_valid(guess, row, col, matrix):
+               # add str() if numbers are in string format
+               matrix[row][col] = str(guess)
+               if solver(matrix):
+                    return True
+          matrix[row][col] = '.'
+
+     return False
