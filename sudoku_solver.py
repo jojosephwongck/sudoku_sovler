96eee507db8bb9626ef91ce5f97976d4858fa2ba
jojosephwongck <111182417+jojosephwongck@users.noreply.github.com>
2022-11-05 15:46:28 +0800

    Update sudoku_solver.py

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,35 +1,12 @@
-A = [["5","3",".",".","7",".",".",".","."],
-     ["6",".",".","1","9","5",".",".","."],
-     [".","9","8",".",".",".",".","6","."],
-     ["8",".",".",".","6",".",".",".","3"],
-     ["4",".",".","8",".","3",".",".","1"],
-     ["7",".",".",".","2",".",".",".","6"],
-     [".","6",".",".",".",".","2","8","."],
-     [".",".",".","4","1","9",".",".","5"],
-     [".",".",".",".","8",".",".","7","9"]]
-
-A2 = [[5, 3,".",".", 7,".",".",".","."],
-     [6,".",".", 1, 9, 5,".",".","."],
-     [".",9,8,".",".",".",".",6,"."],
-     [8,".",".",".",6,".",".",".",3],
-     [4,".",".",8,".",3,".",".",1],
-     [7,".",".",".",2,".",".",".",6],
-     [".",6,".",".",".",".",2,8,"."],
-     [".",".",".",4,1,9,".",".",5],
-     [".",".",".",".",8,".",".",7,9]]
-
 def find_empty(matrix):
      for i in range(9):
           for j in range(9):
-               # empty slot denoted by '.'
+               # find the next empty slot, which is denoted by '.'
                if matrix[i][j] == '.':
                     return i , j
      return None, None
 
 def check_valid(guess, row, col, matrix):
-     # keep if numbers are in string format
-     guess = str(guess)
-
      if guess in matrix[row]:
           return False
 
@@ -52,10 +29,24 @@ def solver(matrix):
 
      for guess in range(1, 10):
           if check_valid(guess, row, col, matrix):
-               # add str() if numbers are in string format
-               matrix[row][col] = str(guess)
+               matrix[row][col] = guess
                if solver(matrix):
                     return True
           matrix[row][col] = '.'
 
      return False
+
+# Here's an example of solving a sodoku puzzle.
+A = [[5, 3,".",".", 7,".",".",".","."],
+     [6,".",".", 1, 9, 5,".",".","."],
+     [".",9,8,".",".",".",".",6,"."],
+     [8,".",".",".",6,".",".",".",3],
+     [4,".",".",8,".",3,".",".",1],
+     [7,".",".",".",2,".",".",".",6],
+     [".",6,".",".",".",".",2,8,"."],
+     [".",".",".",4,1,9,".",".",5],
+     [".",".",".",".",8,".",".",7,9]]
+
+solver(A)
+for row in A:
+     print(row)
